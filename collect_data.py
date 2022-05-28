c26979bd763be72bc9287f4d0a28de7edaa382f1
Shardul Vaidya <cam.v737@gmail.com>
2022-05-28 09:39:33 -0400

    added in plan for data collection system

---
+++ collect_data.py
@@ -0,0 +1,22 @@
+# Basically
+# This is the idea
+# On button 1 press - start data collection (stream camera to file + stream radar to file)
+# On button 2 press - stop data collection
+
+
+# pseudocode
+# on launch
+#   open serial interface + open camera interface in TWO seperate threads
+#   BUT do NOT start recording
+# on gpio rising edge:
+#   start serial recording
+#   in this thread we:  read serial, convert to CSV
+#   start camera recording
+#   in this thread we: read camera frame, store frame locally
+
+# what we need:
+#   Serial Reader class: launch/open_serial() | process_frame() | close_serial()
+#           save_frame()
+#   Camera Reader class: launch/open_camera() | process_frame() | close_camera()
+#           save_frame()
+
